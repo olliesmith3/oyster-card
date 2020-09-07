fc2af5fbfc5a382de2330315a6bb3f54e6ed2e47
Student <student@Makerss-MacBook-Air.local>
2020-09-07 15:36:32 +0100

    Challenge 5

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -3,5 +3,9 @@ attr_reader :balance
   def initialize
       @balance = 0
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
   
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'checks starting balance is 0' do
     expect(subject.balance).to eq 0 
   end 
+
+  it 'adds money to the card' do
+    expect { subject.top_up(10) }.to change(subject, :balance).by(10)
+  end
 end 
