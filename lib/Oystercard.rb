3e4c319b4f2f19a13d210ebf27c033610196aad6
Student <student@Makerss-MacBook-Air.local>
2020-09-09 14:19:12 +0100

    updated from yesterday

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,32 +1,46 @@
 class Oystercard
-attr_reader :balance, :in_journey  
-Limit = 90 
-MINIMUM_FARE = 1
-  def initialize
-      @balance = 0
-      @in_journey = false
+
+  attr_reader :balance, :entry_station, :exit_station, :history
+
+  DEFAULT_VALUE = 15
+  DEFAULT_LIMIT = 90
+  MINIMUM_FARE = 1
+
+  def initialize(balance = DEFAULT_VALUE)
+    @balance = balance
+    @history = []
   end
 
   def top_up(amount)
-    raise "Limit is £#{Limit}" if @balance + amount > Limit 
+    fail "Limit is £#{DEFAULT_LIMIT}" if @balance + amount > DEFAULT_LIMIT
     @balance += amount
   end
 
+  def touch_in(station)
+    fail "Minimum fare is £#{MINIMUM_FARE}" if @balance < MINIMUM_FARE
+    @entry_station = station
+  end
+
+  def touch_out(station, amount = MINIMUM_FARE)
+    deduct(amount)
+    @exit_station = station
+    @history << { entry_station: @entry_station, exit_station: @exit_station }
+    @entry_station = nil
+  end
+
+  def in_journey?
+    @entry_station != nil ? true : false
+  end
+
+  def show_history
+    @history.each do |element|
+      puts "Entry station: #{element[:entry_station]}, Exit station: #{element[:exit_station]}"
+    end
+  end
+
+  private
   def deduct(amount)
     @balance -= amount
   end
-
-  def touch_in
-    raise "Your balance is below the minimum fare" if @balance < MINIMUM_FARE
-    @in_journey = true
-  end 
-
-  def touch_out
-    @in_journey = false
-  end 
-
-  def in_journey?
-    @in_journey
-  end 
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,58 +1,76 @@
-require 'Oystercard'
+require 'oystercard.rb'
 
 describe Oystercard do
-  it 'checks starting balance is 0' do
-    expect(subject.balance).to eq 0 
-  end 
 
-  it 'adds money to the card' do
-    expect { subject.top_up(10) }.to change(subject, :balance).by(10)
+  let(:oyster)            {   Oystercard.new  }
+  let(:liverpool_street)  { "liverpool_street"  }
+  let(:bow_road)          { "bow_road"   }
+
+  it  "checks that the balance of oyster card is equal to the defualt" do
+  expect(oyster.balance).to eq (15)
   end
 
-  it 'throws error if we topup beyond £90' do
-    expect {subject.top_up(100)}.to raise_error "Limit is £90"
-  end 
-
-  it 'allows us to deduct money from a card' do
-    subject.top_up(20)
-    expect { subject.deduct(7) }.to change{ subject.balance }.by -7
+  it "checks that you can top up the card with a specific value" do
+  expect {oyster.top_up(10)}.to change{oyster.balance}.by 10
   end
 
-  describe "#touch_in" do
-    it 'changes instance variable to true' do
-      subject.top_up(10)
-      subject.touch_in
-      expect(subject.in_journey).to eq true  
-    end 
+  it "sends an error message if the card is topped up with more than £90" do
+    expect {oyster.top_up(76)}.to raise_error "Limit is £90"
+  end
 
-    it 'won\'t allow you to touch in if your balance is below the minimum fare' do
-      expect{ subject.touch_in }.to raise_error "Your balance is below the minimum fare"
+  it "checks that you can deduct money from the balance" do
+    expect {oyster.touch_out(bow_road, 10)}.to change{oyster.balance}.by -10
+  end
+
+  describe 'touch_in' do
+    it "checks that the oyster card has been touched in" do
+      oyster.touch_in(liverpool_street)
+      expect(oyster.in_journey?).to eq true
+    end
+
+    it 'won\'t allow me to touch in if the balance is less than £1' do
+      cloyster = Oystercard.new(0)
+      expect { cloyster.touch_in(liverpool_street) }.to raise_error "Minimum fare is £1"
+    end
+
+    it 'remembers the station it touched in at' do
+      oyster.touch_in(liverpool_street)
+      expect(oyster.entry_station).to eq liverpool_street
     end
   end
 
-  describe "#touch_out" do
-    it 'changes instance variable to false' do
-      subject.top_up(10)
-      subject.touch_in
-      subject.touch_out
-      expect(subject.in_journey).to eq false  
-    end 
+  describe 'touch out' do
+    it "checks that the oyster card has been touched out" do
+      oyster.touch_in(liverpool_street)
+      oyster.touch_out(bow_road)
+      expect(oyster.in_journey?).to eq false
+    end
+    it "checks that the balance has been deducted by the minimum fare" do
+      oyster.touch_in(liverpool_street)
+      expect { oyster.touch_out(bow_road, Oystercard::MINIMUM_FARE) }.to change{oyster.balance}.by -Oystercard::MINIMUM_FARE
+    end
+
+    it 'forgets the entry station when it touches out' do
+      oyster.touch_in(liverpool_street)
+      oyster.touch_out(bow_road)
+      expect(oyster.entry_station).to eq nil
+    end
   end
 
-  describe "#in_journey?" do 
-    it 'checking if user is in a journey' do
-      subject.top_up(10)
-      subject.touch_in
-      expect(subject.in_journey?).to eq true 
+  describe 'in_journey?' do
+    it "checks whether the card is in use or not" do
+      oyster.touch_in(liverpool_street)
+      expect(oyster.in_journey?).to be_truthy
     end
 
-    it 'check user isn\'t on a journey' do 
-      subject.top_up(10)
-      subject.touch_in
-      subject.touch_out 
-      expect(subject.in_journey?).to eq false
+    it "checks whether the card is in use or not" do
+      oyster.touch_out(bow_road)
+      expect(oyster.in_journey?).to be_falsey
     end
-  end 
-
-
+  end
+  it "lists the entry and exit stations" do
+    oyster.touch_in(liverpool_street)
+    oyster.touch_out(bow_road)
+    expect { oyster.show_history }.to output("Entry station: liverpool_street, Exit station: bow_road\n").to_stdout
+  end
 end
